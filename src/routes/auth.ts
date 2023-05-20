a03b58bff69f0d04668cf0e9c72d2f2801bbd6a8
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-20 20:08:04 -0300

    feat: verificação de jwt em rotas memorias

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('/register', async (request, reply) => {
+  app.post('/register', async (request) => {
     const bodySchema = z.object({
       code: z.string(),
     })
