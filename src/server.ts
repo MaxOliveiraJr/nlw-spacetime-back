d2f000aecb1f435d6d5cc934919c67fa32d45424
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-20 00:12:26 -0300

    feat: validade acesso pelo git hub

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
-import fastifyCors from '@fastify/cors'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 app.register(cors, {
@@ -8,7 +9,7 @@ app.register(cors, {
 })
 
 app.register(memoriesRoutes)
-
+app.register(authRoutes)
 app
   .listen({
     port: 3333,
